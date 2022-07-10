77ea0529c45bcd3e0a49be993df1bfb80180c9be
deb boro <debajit.boro666@gmail.com>
2022-07-10 16:24:19 -0400

    updated

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const fs = require('fs')
 const path = require('path')
+const cors = require('cors')
 
 const { animals } = require('./data/animals.json')
 const PORT = process.env.PORT || 3001
 const app = express()
-
+//blocked by CORS policy: Cross origin requests are only supported for protocol schemes
+app.use(cors())
+app.use(express.static('public'))
 //parsing incoming string or array data - middleware
 app.use(express.urlencoded({ extended: true }))
-
 //parse incoming JSON data - middleware
 app.use(express.json())
 
@@ -124,6 +126,21 @@ app.post('/api/animals', (req, res) => {
     const animal = createNewAnimal(req.body, animals)
     res.json(animal)
   }
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'))
+})
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'))
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
 //listen
